aaa39c0382ada16bec21879eb52fba14f595a27a
Jesiel Palacios <jopalacioss@unac.edu.co>
2021-10-06 02:49:01 -0500

    Segunda prueba unitaria.

--- src/main/java/EjerciciosClase/Operaciones.java
+++ src/main/java/EjerciciosClase/Operaciones.java
@@ -2,9 +2,10 @@ package EjerciciosClase;
 
 public class Operaciones {
 
-	public static int Suma(int numeroUno, int numeroDos) {
-    // TODO Auto-generated method stub
-    return 8;
+	public int Suma(int numeroUno, int numeroDos) {
+        // TODO Auto-generated method stub
+        int resultado = numeroUno + numeroDos;
+        return resultado;
 		
 	}
   
--- src/test/java/EjerciciosClase/OperacionesTest.java
+++ src/test/java/EjerciciosClase/OperacionesTest.java
@@ -13,13 +13,33 @@ public class OperacionesTest {
     int numeroUno = 5;
     int numeroDos = 3;
 
+    Operaciones operaciones = new Operaciones();
+
     // Act
     // A dónde voy a ir?
     int resultado;
-    resultado = Operaciones.Suma(numeroUno, numeroDos);
+    resultado = operaciones.Suma(numeroUno, numeroDos);
 
     // Assert
     // Validar que está bien o mal.
     assertEquals(8, resultado);
+
+    // Toda función debe tener más de una validación, tanto éxito como fallo
+    // y validar multiples veces o diferentes casos de uso.
+  }
+
+  @Test
+  public void reciboCuatroYTresYObtengoSiete() {
+    // Arrange  
+    int numeroUno = 4;
+    int numeroDos = 3;
+    Operaciones operaciones = new Operaciones();
+
+    // Act
+    int resultado;
+    resultado = operaciones.Suma(numeroUno, numeroDos);
+
+    // Assert
+    assertEquals(7, resultado);
   }
 }
